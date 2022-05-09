7b49337635569156fa3ff2c7a7fcef91484edfe8
CristhianGarciaFernandez <cgarciafern21@students.kgi.edu>
2022-05-08 21:53:51 -0700

    chat

--- scr/scripts.js
+++ scr/scripts.js
@@ -19,7 +19,7 @@ window.initMap = initMap;
 // Chat box JS
 $('#send-message').click(function () {
   // Get message from input box
-  var message = $('#chat-message').val();
+  var message = $('#chat-messages').val();
   var messageHTML = '<p class="chat-messages">' + message + '</p>';
   // Append formatted HTML of message to the chat box
   $('.chat-box').append(messageHTML);
